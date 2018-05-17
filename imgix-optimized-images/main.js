9758d426ed3ba036c94bbc615c782f68655567d6
Sean C Davis <scdavis41@gmail.com>
2018-05-17 13:30:55 -0400

    [imgix-optimized-images] Account for object-fit and margin

--- imgix-optimized-images/main.js
+++ imgix-optimized-images/main.js
@@ -9,8 +9,11 @@ window.CRDS = window.CRDS || {};
 
 CRDS.ImageOptimizer = function(img) {
   this.timeToFade = 250;
+  this.processing = false;
 
   this.img = $(img);
+
+  if (this.img[0].complete) { this.renderTempImg() }
   this.img.on('load', $.proxy(this.renderTempImg, this));
 };
 
@@ -23,22 +26,24 @@ CRDS.ImageOptimizer.init = function() {
 CRDS.ImageOptimizer.prototype.constructor = CRDS.ImageOptimizer;
 
 CRDS.ImageOptimizer.prototype.renderTempImg = function() {
+
+  if (this.processing) { return false; }
+
+  this.processing = true;
+
   this.tmpImg = this.img.clone();
 
   this.tmpImg.css({
     position: 'absolute',
-    top: this.img.offset().top,
-    left: this.img.offset().left,
+    top: this.img.offset().top - parseInt(this.img.css('margin-top')),
+    left: this.img.offset().left - parseInt(this.img.css('margin-left')),
     width: this.img.width(),
     height: this.img.height(),
     zIndex: '-1'
   });
 
-  this.tmpImg.attr({
-    'ix-src': this.img.attr('src'),
-    'class': 'img-responsive'
-  });
-
+  this.tmpImg.attr('ix-src', this.img.attr('src'));
+  this.tmpImg.addClass('img-responsive');
   this.tmpImg.removeAttr('src');
   this.tmpImg.removeAttr('data-optimize-img');
 
