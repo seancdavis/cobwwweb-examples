3f3c5a9fd1d58f30c0844d9c49b21c30f28ed5d1
Sean C Davis <scdavis41@gmail.com>
2018-05-17 15:29:01 -0400

    [imgix-optimized-images] Account for background images

--- imgix-optimized-images/main.js
+++ imgix-optimized-images/main.js
@@ -1,11 +1,6 @@
-// TODO:
-//
-// 1. Inline-block images
-// 2. Inline images
-// 3. Block images
-// 4. Background images
-
 window.CRDS = window.CRDS || {};
+
+// ---------------------------------------- | ImageOptimizer
 
 CRDS.ImageOptimizer = function(img) {
   this.timeToFade = 250;
@@ -13,21 +8,15 @@ CRDS.ImageOptimizer = function(img) {
 
   this.img = $(img);
 
-  if (this.img[0].complete) { this.renderTempImg() }
-  this.img.on('load', $.proxy(this.renderTempImg, this));
+  if (this.img[0].complete) { this.renderTmpImg() }
+  this.img.on('load', $.proxy(this.renderTmpImg, this));
 };
-
-CRDS.ImageOptimizer.init = function() {
-  $('img[data-optimize-img]').each(function(idx, img) {
-    new CRDS.ImageOptimizer(img);
-  });
-}
 
 CRDS.ImageOptimizer.prototype.constructor = CRDS.ImageOptimizer;
 
-CRDS.ImageOptimizer.prototype.renderTempImg = function() {
+CRDS.ImageOptimizer.prototype.renderTmpImg = function() {
 
-  if (this.processing) { return false; }
+  if (this.processing) { return }
 
   this.processing = true;
 
@@ -55,6 +44,7 @@ CRDS.ImageOptimizer.prototype.renderTempImg = function() {
 }
 
 CRDS.ImageOptimizer.prototype.transitionImg = function() {
+  if (!this.img) { return }
   this.img.fadeTo(this.timeToFade, 0);
 
   setTimeout($.proxy(function() {
@@ -62,7 +52,82 @@ CRDS.ImageOptimizer.prototype.transitionImg = function() {
     this.tmpImg.attr('class', this.img.attr('class'));
     this.tmpImg.insertAfter(this.img);
     this.img.remove();
+    this.img = undefined;
   }, this), this.timeToFade);
+}
+
+// ---------------------------------------- | BgImageOptimizer Constructor
+
+CRDS.BgImageOptimizer = function(bgEl) {
+  this.timeToFade = 250;
+  this.processing = false;
+
+  this.bgEl = $(bgEl);
+  this.imgUrl = this.bgEl.css('background-image').replace('url(', '').replace(')', '').replace(/\"/gi, "");
+
+  $('<img>').on('load', $.proxy(this.renderTmpImg, this)).attr('src', this.imgUrl);
+};
+
+CRDS.BgImageOptimizer.prototype.constructor = CRDS.BgImageOptimizer;
+
+CRDS.BgImageOptimizer.prototype.renderTmpImg = function() {
+  this.tmpPlacholderBgEl = this.bgEl.clone();
+  this.tmpPlacholderBgEl.html('');
+
+  this.tmpPlacholderBgEl.css({
+    position: 'absolute',
+    top: this.bgEl.offset().top - parseInt(this.bgEl.css('margin-top')),
+    left: this.bgEl.offset().left - parseInt(this.bgEl.css('margin-left')),
+    width: this.bgEl.width(),
+    height: this.bgEl.height(),
+    zIndex: '-1'
+  });
+
+  $('body').prepend(this.tmpPlacholderBgEl);
+
+  this.bgEl.css('background-image', '');
+
+  this.tmpFullSizeBgEl = this.tmpPlacholderBgEl.clone();
+
+  var newUrl = new URL(this.imgUrl);
+  if (newUrl.searchParams.get('w')) {
+    newUrl.searchParams.set('w', this.bgEl.outerWidth());
+  }
+  if (newUrl.searchParams.get('h')) {
+    newUrl.searchParams.set('h', this.bgEl.outerHeight());
+  }
+
+  this.tmpFullSizeBgEl.css({
+    backgroundImage: 'url("' + newUrl.href + '")',
+    zIndex: '-2'
+  });
+
+  $('body').prepend(this.tmpFullSizeBgEl);
+  $('<img>').on('load', $.proxy(this.transitionImg, this)).attr('src', newUrl.href);
+}
+
+CRDS.BgImageOptimizer.prototype.transitionImg = function() {
+  this.tmpPlacholderBgEl.fadeTo(this.timeToFade, 0);
+
+  setTimeout($.proxy(function() {
+    this.bgEl.css('background-image', this.tmpFullSizeBgEl.css('background-image'));
+    this.tmpPlacholderBgEl.remove();
+    this.tmpFullSizeBgEl.remove();
+    this.tmpPlacholderBgEl = undefined;
+    this.tmpFullSizeBgEl = undefined;
+  }, this), this.timeToFade);
+}
+
+// ---------------------------------------- | Initializer
+
+CRDS.ImageOptimizer.init = function() {
+  $('img[data-optimize-img]').each(function(idx, img) {
+    new CRDS.ImageOptimizer(img);
+  });
+
+  $('[data-optimize-bg-img]').each(function(idx, bgEl) {
+    new CRDS.BgImageOptimizer(bgEl);
+  });
 }
 
 $(document).ready(CRDS.ImageOptimizer.init);
