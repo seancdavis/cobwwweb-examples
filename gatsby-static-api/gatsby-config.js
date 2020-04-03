5fc949ff114cc6ad27ffa86945223911afe8a1e9
Sean C Davis <scdavis41@gmail.com>
2020-04-03 07:26:55 -0400

    Add static API

--- gatsby-static-api/gatsby-config.js
+++ gatsby-static-api/gatsby-config.js
@@ -13,6 +13,14 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `earworms`,
+        path: `${__dirname}/data/earworms`,
+      },
+    },
+    `gatsby-transformer-yaml`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
--- gatsby-static-api/gatsby-node.js
+++ gatsby-static-api/gatsby-node.js
@@ -1,7 +1,47 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+const fs = require("fs")
 
-// You can delete this file if you're not using it
+exports.onPostBuild = async ({ graphql }) => {
+  graphql(`
+    {
+      earworms: allEarwormsYaml {
+        edges {
+          node {
+            song_id
+            date(formatString: "YYYY-MM-DD")
+            artist
+            title
+            spotify_url
+          }
+        }
+      }
+    }
+  `).then(result => {
+    // A reference to where we are going to put the files.
+    const earwormsPath = "./public/earworms"
+
+    // Collect the data for all earworms
+    const earworms = result.data.earworms.edges.map(({ node }) => node)
+
+    // Query result for the index file.
+    const allEarworms = {
+      result: earworms,
+      meta: {
+        count: earworms.length,
+      },
+    }
+
+    // Write the index file.
+    fs.writeFileSync(`${earwormsPath}.json`, JSON.stringify(allEarworms))
+
+    // Create directory for individual files if it doesn't exist.
+    if (!fs.existsSync(earwormsPath)) fs.mkdirSync(earwormsPath)
+
+    // Write individual files.
+    earworms.map(worm => {
+      fs.writeFileSync(
+        `${earwormsPath}/${worm.song_id}.json`,
+        JSON.stringify(worm)
+      )
+    })
+  })
+}
